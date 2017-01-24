6d7b9b15dcfa0fed6a14089cabb7d46413df1aa6
reyntjesr <r.reijntjes@gmail.com>
2017-01-24 22:43:11 +0100

    Fixed reading and writng compound arrays. Thanks to Sharpwood's response to an issue I opened

--- Hdf5DotNetTools/Hdf5Compounds.cs
+++ Hdf5DotNetTools/Hdf5Compounds.cs
@@ -20,46 +20,83 @@ namespace Hdf5DotNetTools
         // information: https://www.hdfgroup.org/ftp/HDF5/examples/examples-by-api/hdf5-examples/1_8/C/H5T/h5ex_t_cmpd.c
         //or: https://www.hdfgroup.org/HDF5/doc/UG/HDF5_Users_Guide-Responsive%20HTML5/index.html#t=HDF5_Users_Guide%2FDatatypes%2FHDF5_Datatypes.htm%3Frhtocid%3Dtoc6.5%23TOC_6_8_Complex_Combinationsbc-22
 
-        public static int WriteCompounds<T>(hid_t groupId, string name, IEnumerable<T> array) where T : struct
+        public static int WriteCompounds<T>(hid_t groupId, string name, IEnumerable<T> list) //where T : struct
         {
-            hid_t memId=0, filetype=0;
             Type type = typeof(T);
-            var fields = type.GetFields();
-            //array = changeStrings(array, fields);
-            //var compoundSizearraySize = Marshal.SizeOf(array);
-            ulong[] dims = new ulong[] { (ulong)array.Count() };
+            var size = Marshal.SizeOf(type);
+            var cnt = list.Count();
 
-            calcCompoundSize(type,false,ref memId);
+            var typeId = create_type(type);
 
-            calcCompoundSize(type,true,ref filetype);
+            var log10 = (int)Math.Log10(cnt);
+            ulong pow = (ulong)Math.Pow(10, log10);
+            ulong c_s = Math.Min(1000, pow);
+            ulong[] chunk_size = new ulong[] { c_s };
+
+            ulong[] dims = new ulong[] { (ulong)cnt };
+
+            long dcpl = 0;
+            if (list.Count() == 0 || log10 == 0) { }
+            else
+            {
+                dcpl = create_property(chunk_size);
+            }
 
             // Create dataspace.  Setting maximum size to NULL sets the maximum
             // size to be the current size.
-            var spaceId = H5S.create_simple(1, dims, null);
+            var spaceId = H5S.create_simple(dims.Length, dims, null);
 
             // Create the dataset and write the compound data to it.
-            var datasetId = H5D.create(groupId, name, filetype, spaceId, H5P.DEFAULT, H5P.DEFAULT, H5P.DEFAULT);
+            var datasetId = H5D.create(groupId, name, typeId, spaceId, H5P.DEFAULT, dcpl);
+
+            IntPtr p = Marshal.AllocHGlobal(size * (int)dims[0]);
 
             var ms = new MemoryStream();
             BinaryWriter writer = new BinaryWriter(ms);
-            foreach (var strct in array)
+            foreach (var strct in list)
                 writer.Write(getBytes(strct));
             var bytes = ms.ToArray();
 
             GCHandle hnd = GCHandle.Alloc(bytes, GCHandleType.Pinned);
-            var statusId = H5D.write(datasetId, memId, H5S.ALL, H5S.ALL, H5P.DEFAULT,
-                    hnd.AddrOfPinnedObject());
+            var statusId = H5D.write(datasetId, typeId, spaceId, H5S.ALL, 
+                H5P.DEFAULT, hnd.AddrOfPinnedObject());
+
             hnd.Free();
             /*
              * Close and release resources.
              */
             H5D.close(datasetId);
             H5S.close(spaceId);
-            H5T.close(filetype);
+            H5T.close(typeId);
+            H5P.close(dcpl);
+            Marshal.FreeHGlobal(p);
             return statusId;
         }
 
+        private static long create_property(ulong[] chunk_size)
+        {
+            var dcpl = H5P.create(H5P.DATASET_CREATE);
+            H5P.set_layout(dcpl, H5D.layout_t.CHUNKED);
+            H5P.set_chunk(dcpl, chunk_size.Length, chunk_size);
+            H5P.set_deflate(dcpl, 6);
+            return dcpl;
+        }
 
+        private static long create_type(Type t)
+        {
+            var size = Marshal.SizeOf(t);
+            var float_size = Marshal.SizeOf(typeof(float));
+            var int_size = Marshal.SizeOf(typeof(int));
+            var typeId = H5T.create(H5T.class_t.COMPOUND, new IntPtr(size));
+
+            var compoundInfo = Hdf5.GetCompoundInfo(t);
+            foreach (var cmp in compoundInfo)
+            {
+                //Console.WriteLine(string.Format("{0}  {1}", cmp.name, cmp.datatype));
+                H5T.insert(typeId, cmp.name, Marshal.OffsetOf(t, cmp.name), cmp.datatype);
+            }
+            return typeId;
+        }
         private static IEnumerable<T> changeStrings<T>(IEnumerable<T> array, FieldInfo[] fields) where T : struct
         {
             foreach (var info in fields)
@@ -74,12 +111,12 @@ namespace Hdf5DotNetTools
 
 
         ///
-        private static int calcCompoundSize(Type type,bool useIEEE, ref hid_t id)
+        private static int calcCompoundSize(Type type, bool useIEEE, ref hid_t id)
         {
             // Create the compound datatype for the file.  Because the standard
             // types we are using for the file may have different sizes than
             // the corresponding native types
-            var compoundInfo = Hdf5.GetCompoundInfo(type,useIEEE);
+            var compoundInfo = Hdf5.GetCompoundInfo(type, useIEEE);
             var curCompound = compoundInfo.Last();
             var compoundSize = curCompound.offset + curCompound.size;
             //Create the compound datatype for memory.
@@ -134,11 +171,12 @@ namespace Hdf5DotNetTools
         public static IEnumerable<T> ReadCompounds<T>(hid_t groupId, string name) where T : struct
         {
             Type type = typeof(T);
-            hid_t memId = 0;
+            hid_t typeId = 0;
             // open dataset
             var datasetId = H5D.open(groupId, name);
 
-            var compoundSize = calcCompoundSize(type,false,ref memId);
+            typeId = create_type(type);
+            var compoundSize = Marshal.SizeOf(type);
 
             /*
              * Get dataspace and allocate memory for read buffer.
@@ -149,10 +187,10 @@ namespace Hdf5DotNetTools
             var ndims = H5S.get_simple_extent_dims(spaceId, dims, null);
             int rows = Convert.ToInt32(dims[0]);
 
-            byte[] bytes = new byte[rows*compoundSize];
+            byte[] bytes = new byte[rows * compoundSize];
             // Read the data.
             GCHandle hnd = GCHandle.Alloc(bytes, GCHandleType.Pinned);
-            H5D.read(datasetId, memId, H5S.ALL,H5S.ALL, H5P.DEFAULT, hnd.AddrOfPinnedObject());
+            H5D.read(datasetId, typeId, spaceId, H5S.ALL, H5P.DEFAULT, hnd.AddrOfPinnedObject());
             hnd.Free();
             int counter = 0;
             IEnumerable<T> strcts = Enumerable.Range(1, rows).Select(i =>
